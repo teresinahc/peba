7ec842a63f08d911d2131dd520c3cb2514b0d42a
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2015-02-21 15:14:03 -0300

    Adicionando scope aos deputados

--- app/models/deputado.rb
+++ app/models/deputado.rb
@@ -1,3 +1,12 @@
 class Deputado < ActiveRecord::Base
-   has_many :despesas
+
+  has_many :despesas
+
+  scope :maiores_despesas, -> { 
+     joins(:despesas) 
+    .select('deputados.id', 'deputados.nome', 'sum(despesas.valor_liquido) as total_despesas')
+    .group('deputados.nome')
+    .order('total_despesas desc')
+  }
+
 end
