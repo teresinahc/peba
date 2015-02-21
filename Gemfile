732c21054828431a4b8651e244db62c127cf58e4
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2015-02-21 16:08:16 -0300

    Melhorando scope de busca por despesas dos deputados e adicionando rota root

--- Gemfile
+++ Gemfile
@@ -40,10 +40,11 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  gem 'spring', '1.3.2'
 end
 
 gem 'nokogiri'
 gem 'will_paginate'
 gem 'bootstrap-sass', '~> 3.3.3'
 gem 'rails-i18n'
+
--- app/models/deputado.rb
+++ app/models/deputado.rb
@@ -1,12 +1,15 @@
 class Deputado < ActiveRecord::Base
-  
-  has_many :despesas
 
-  scope :maiores_despesas, -> { 
+  has_many :despesas
+  
+  scope :com_despesas, -> { 
      joins(:despesas) 
-    .select('deputados.id', 'deputados.nome', 'sum(despesas.valor_liquido) as total_despesas')
+    .select('*', 'sum(despesas.valor_liquido) as total_despesas')
     .group('deputados.nome')
-    .order('total_despesas desc')
+  }
+
+  scope :maiores_despesas, -> {
+    com_despesas.order('total_despesas desc')
   }
 
 end
