25f04631bee51f09d393850ea258d26f7d08cada
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2015-02-27 23:44:48 -0300

    Melhorando SEO e compartilhamento de informações

--- app/controllers/deputados_controller.rb
+++ app/controllers/deputados_controller.rb
@@ -12,6 +12,18 @@ class DeputadosController < ApplicationController
   def show
   	@deputado = Deputado.com_total_despesas.find(params[:id])
   	@despesas = @deputado.despesas.todos(params[:q], params[:page])
+    
+    @page_info = {
+      title: 'Deputado: ' + @deputado.nome.titleize
+    }
+
+    @opengraph = {
+      title:      'Deputado: ' + @deputado.nome.titleize,
+      type:       'website',
+      url:        url_for(:only_path => false, :overwrite_params=>nil),
+      image:      @deputado.url_foto,
+      site_name: 'Peba'
+    }
   end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,18 @@ class HomeController < ApplicationController
   layout 'search'
 
   def index
+    @page_info = {
+      title: 'Indexador de Dados Públicos',
+      description:  'O Peba é sistema que indexa em fonte única informações relacionadas às despesas de vários entidades governamentais com suporte para filtros e buscas avançadas.'
+    }
+    @opengraph = {
+      title:        'Peba - Indexador de Dados Públicos',
+      type:         'website',
+      url:          request.base_url,
+      image:        request.base_url + view_context.image_path('peba_logo_og.png'),
+      site_name:    'Peba',
+      description:  'O Peba é um sistema que indexa em fonte única informações relacionadas às despesas de vários entidades governamentais com suporte para filtros e buscas avançadas.'
+    }
   end
 
   def aleatorio
