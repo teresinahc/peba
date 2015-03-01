54cc5d1192087fb660af36bc598bac737aa6c649
Renato Filho <renatosousafilho@gmail.com>
2015-03-01 08:34:33 -0300

    classsificando despesas por descricao

--- app/models/despesa.rb
+++ app/models/despesa.rb
@@ -1,8 +1,20 @@
 class Despesa < ActiveRecord::Base
   
   scope :total_gasto, -> { sum(:valor_liquido) }
-  scope :tipos, -> { select("distinct(descricao)") }
+  scope :tipos, -> { select("distinct(descricao)").map { |despesa| despesa.descricao } }
+  scope :totais_por_descricao, -> { 
+  	select("descricao,sum(valor_liquido) as total_liquido,sum(valor_documento) as total")
+  	.group("descricao")
+  	.map do |despesa|
+  		{
+  			:descricao=>despesa.descricao,
+  			:total_liquido=>despesa.total_liquido,
+  			:total => despesa.total
+  		}
+  	end
+  }
   scope :mais_novos,  -> { order('data_emissao desc') }
+  scope :por_descricao,  ->(descricao) { where(:descricao=>descricao) }
 
   belongs_to :deputado
 
@@ -10,4 +22,5 @@ class Despesa < ActiveRecord::Base
     mais_novos.paginate(:page => current_page, :per_page => per_page)
   end
 
+ 
 end
