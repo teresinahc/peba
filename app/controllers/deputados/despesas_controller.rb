c7f1df32fac7ec616ade0c6f2e09422874c499f9
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2015-03-01 22:03:27 -0300

    Adicionando filtros no detalhamento de despesas

--- app/controllers/deputados/despesas_controller.rb
+++ app/controllers/deputados/despesas_controller.rb
@@ -1,8 +1,15 @@
 class Deputados::DespesasController < ApplicationController
-	layout 'search'
-
 	def index
 		@deputado = Deputado.com_total_despesas.find(params[:deputado_id])
-		@despesas = @deputado.despesas
+		@despesas = @deputado.despesas.por_filtro(filter_params).paginate(page: params[:page], per_page: 20)
 	end
+
+  def filter_params
+    filtro = Hash.new
+    filtro[:num_ano]   = params[:num_ano]     unless params[:num_ano].blank?
+    filtro[:num_mes]   = params[:num_mes]     unless params[:num_mes].blank?
+    filtro[:descricao] = params[:descricao] unless params[:descricao].blank?
+    
+    return filtro
+  end
 end
--- app/models/despesa.rb
+++ app/models/despesa.rb
@@ -1,25 +1,33 @@
 class Despesa < ActiveRecord::Base
   
+  belongs_to :deputado
+
   scope :total_gasto, -> { sum(:valor_liquido) }
-  scope :classificacoes, -> { select("distinct(descricao)").map { |classificacao| classificacao.descricao } }
-  scope :totais_por_descricao, -> { 
-  	select("descricao,sum(valor_liquido) as total_liquido,sum(valor_documento) as total")
-  	.group("descricao")
-  	.map do |despesa|
-  		{
-  			:descricao=>despesa.descricao,
-  			:total_liquido=>despesa.total_liquido,
-  			:total => despesa.total
-  		}
-  	end
-  }
   scope :mais_novos,  -> { order('data_emissao desc') }
   scope :por_classificacao,  ->(classificacao) { where(:descricao=>classificacao) }
+  
+  scope :classificacoes, -> { 
+    select("distinct(descricao)").map { |classificacao| classificacao.descricao }
+  }
 
-  belongs_to :deputado
+  scope :totais_por_descricao, -> { 
+    select("descricao,sum(valor_liquido) as total_liquido,sum(valor_documento) as total")
+    .group("descricao")
+    .map do |despesa|
+      {
+        :descricao=>despesa.descricao,
+        :total_liquido=>despesa.total_liquido,
+        :total => despesa.total
+      }
+    end
+  }
 
   def self.todos(query, current_page, per_page = 20)
     mais_novos.paginate(:page => current_page, :per_page => per_page)
+  end
+
+  def self.por_filtro(filtro)
+    mais_novos.where(filtro)
   end
 
  
