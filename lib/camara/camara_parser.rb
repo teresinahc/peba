47d7c38d858ecdb813a9bb1eaa7c9a9e697adc31
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2015-03-07 18:33:48 -0300

    Refatorando crawler

--- lib/camara/camara_parser.rb
+++ lib/camara/camara_parser.rb
@@ -6,51 +6,55 @@ class CamaraParser
     	deputados = document.css("deputado")
     	deputados.map do |deputado|
 	       {
-	         :nome => deputado.css("nome").text,
+	         :nome 						 => deputado.css("nome").text,
 	         :nome_parlamentar => deputado.css("nomeParlamentar").text,
-	         :partido => deputado.css("partido").text,
-	         :matricula => deputado.css("matricula").text,
-	         :url_foto => deputado.css("urlFoto").text,
-	         :email => deputado.css("email").text,
-	         :uf => deputado.css("uf").text,
-	         :id_cadastro => deputado.css("ideCadastro").text.to_i
+	         :partido 				 => deputado.css("partido").text,
+	         :matricula 			 => deputado.css("matricula").text,
+	         :url_foto 				 => deputado.css("urlFoto").text,
+	         :email 					 => deputado.css("email").text,
+	         :uf 							 => deputado.css("uf").text,
+	         :id_cadastro 		 => deputado.css("ideCadastro").text.to_i
 	       }
 	   end
 	end
 
 	def self.parse_cota_parlamentar(content)
-		doc = Nokogiri::XML.parse(content)
-    	despesas = doc.css("DESPESA")
-			despesas.map do |despesa|
-				if !despesa.css("ideCadastro").text.blank? and !despesa.css("cpf_cnpj").text.blank? and !despesa.css("numero").text.blank?
+		doc 		  = Nokogiri::XML.parse(content)
+  	despesas  = doc.css("DESPESA")
+		registros	= Array.new
 
-					deputado = Rails.cache.fetch("deputados", expires_in: 4.year) do
-      			Deputado.find(despesa.css("ideCadastro").text)
-    			end
+		despesas.map do |despesa|
+			if !despesa.css("ideCadastro").text.blank? and !despesa.css('txtCNPJCPF').text.blank? and !despesa.css("txtNumero").text.blank?
 
-		  		data_emissao = despesa.css("datEmissao").text
-					{
-			      :matricula => despesa.css("nuCarteiraParlamentar").text,
-			      :nome_parlamentar => despesa.css("txNomeParlamentar").text,
-			      :beneficiario => despesa.css("txtBeneficiario").text,
-			      :descricao => despesa.css("txtDescricao").text,
-			      :data_emissao=> data_emissao.empty? ? '' : Date.parse(data_emissao),
-			      :valor_documento => despesa.css("vlrDocumento").text,
-			      :valor_liquido => despesa.css("vlrLiquido").text,
-			      :valor_glosa => despesa.css("vlrGlosa").text,
-			      :cpf_cnpj => despesa.css("txtCNPJCPF").text,
-			      :id_cadastro => despesa.css("ideCadastro").text.to_i,
-			      :num_mes => despesa.css("numMes").text.to_i,
-			      :num_ano => despesa.css("numAno").text.to_i,
-			      :num_parcela => despesa.css("numParcela").text.to_i,
-			      :num_lote => despesa.css("numLote").text.to_i,
-			      :num_ressarcimento => despesa.css("numRessarcimento").text.to_i,
-			      :num_sub_cota => despesa.css("numSubCota").text.to_i,
-			      :num_especificacao_sub_cota => despesa.css("numEspecificacaoSubCota").text.to_i,
-			      :deputado_id => deputado.id,
-			      :numero => despesa.css("txtNumero").text
-		      }
-	    	end
+				deputado = Rails.cache.fetch("deputados_#{despesa.css('ideCadastro').text}", expires_in: 4.year) do
+    			Deputado.find_by_id_cadastro(despesa.css("ideCadastro").text)
+  			end
+
+	  		data_emissao = despesa.css('datEmissao').text
+				registros << {
+		      :matricula 									=> despesa.css('nuCarteiraParlamentar').text,
+		      :nome_parlamentar 					=> despesa.css('txNomeParlamentar').text,
+		      :beneficiario 							=> despesa.css('txtBeneficiario').text,
+		      :descricao 									=> despesa.css('txtDescricao').text,
+		      :data_emissao								=> data_emissao.empty? ? '' : Date.parse(data_emissao),
+		      :valor_documento 						=> despesa.css('vlrDocumento').text,
+		      :valor_liquido 							=> despesa.css('vlrLiquido').text,
+		      :valor_glosa 								=> despesa.css('vlrGlosa').text,
+		      :cpf_cnpj 									=> despesa.css('txtCNPJCPF').text,
+		      :id_cadastro 								=> despesa.css('ideCadastro').text.to_i,
+		      :num_mes 										=> despesa.css('numMes').text.to_i,
+		      :num_ano 										=> despesa.css('numAno').text.to_i,
+		      :num_parcela 								=> despesa.css('numParcela').text.to_i,
+		      :num_lote 									=> despesa.css('numLote').text.to_i,
+		      :num_ressarcimento 					=> despesa.css('numRessarcimento').text.to_i,
+		      :num_sub_cota 							=> despesa.css('numSubCota').text.to_i,
+		      :num_especificacao_sub_cota => despesa.css('numEspecificacaoSubCota').text.to_i,
+		      :deputado_id 								=> deputado.id,
+		      :numero 										=> despesa.css('txtNumero').text
+	      } unless deputado.nil?
     	end
+  	end
+
+  	return registros
 	end
 end
--- lib/camara/camara_updater.rb
+++ lib/camara/camara_updater.rb
@@ -2,13 +2,15 @@ class CamaraUpdater
 	# recebe a arrray de hashes
 	def self.update_deputados(deputados)
 		deputados.each do |deputado|
-      Deputado.create(deputado)
+      registro = Deputado.where(id_cadastro: deputado[:id_cadastro]).first_or_create
+      registro.update(deputado)
     end
 	end
 
 	def self.update_cotas_parlamentares(lista_cotas)
 		lista_cotas.each do |cota| 
-      Despesa.create(cota)
+      registro = Despesa.where(cpf_cnpj: cota[:cpf_cnpj], numero: cota[:numero], id_cadastro: cota[:id_cadastro]).first_or_create
+      registro.update(cota)
     end
 	end
 end
