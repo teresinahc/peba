b11e5b80f51dd5f26aca6c69128e267e6bafecc3
Julio Betta <juliobetta@gmail.com>
2015-04-19 10:52:34 -0300

    melhorar a mensagem de total de resultados retornados pela busca

--- spec/controllers/deputados_spec.rb
+++ spec/controllers/deputados_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe DeputadosController, type: :controller do
-  before(:each) { @deputado = Deputado.create! FactoryGirl.attributes_for(:deputado) }
+  let(:deputado) { Deputado.create! FactoryGirl.attributes_for(:deputado) }
 
   render_views
 
   describe 'GET #index' do
     context 'via json' do
       it 'deve listar os deputados e exibe os atributos corretamente' do
-        get :index, format: :json
+        get :index, params: {q: deputado.nome}, format: :json
 
         json = JSON.parse(response.body)
         espero_que(json).tenha %w(deputados total)
@@ -20,7 +20,7 @@ describe DeputadosController, type: :controller do
     end
 
     context 'via html' do
-      before(:each) { get :index}
+      before(:each) { get :index, params: {q: deputado.nome}}
 
       it 'deve exibir total de votos' do
         expect(response.body).to have_content('votos')
@@ -38,13 +38,13 @@ describe DeputadosController, type: :controller do
     context 'busca' do
       it 'deve retornar resultado para cada parametro' do
         [:nome, :nome_parlamentar, :partido, :matricula, :url_foto, :email, :uf].each do |attr|
-          get :index, params: {q: @deputado[attr]}, format: :json
+          get :index, params: {q: deputado[attr]}, format: :json
           expect(response.body).to_not be_blank
         end
       end
 
       it 'nao deve exibir logo no cover' do
-        get :index, params: {q: @deputado[:nome]}
+        get :index, params: {q: deputado.nome}
         expect(response.body).to_not have_selector('#logo')
       end
     end
@@ -52,11 +52,11 @@ describe DeputadosController, type: :controller do
 
 
   describe 'GET #show' do
-    before(:each) { @deputado.despesas << FactoryGirl.build(:despesa) }
+    before(:each) { deputado.despesas << FactoryGirl.build(:despesa) }
 
     context 'via json' do
       it 'deve exibir o deputado e os atributos corretamente' do
-        get :show, id: @deputado.id, format: :json
+        get :show, id: deputado.id, format: :json
 
         json = JSON.parse(response.body)
         espero_que(json).tenha %w(id nome email partido uf url_foto total_despesas total_votos fonte despesas
@@ -69,11 +69,11 @@ describe DeputadosController, type: :controller do
 
     context 'via html' do
       it 'deve exibir o total de votos, votos validos e situacao' do
-        get :show, id: @deputado.id
+        get :show, id: deputado.id
 
         expect(response.body).to have_content('votos')
         expect(response.body).to have_content('votos válidos')
-        expect(response.body).to have_content(@deputado.situacao_candidatura)
+        expect(response.body).to have_content(deputado.situacao_candidatura)
       end
     end
   end
