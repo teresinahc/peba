9f1f2327367d692b8691866e539e133124417aa5
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2016-10-13 22:36:58 -0300

    Config timeout puma server

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
+worker_timeout Integer(ENV['WORK_TIMEOUT'] || 1200)
 
 preload_app!
 
